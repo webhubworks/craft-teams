36233b175ddc77055d69bb61df5bafe4db21682d
Johannes Ahrndt <johannes.ahrndt@rostock-ahoi.de>
2024-10-08 21:20:24 +0200

    first commit

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# Release Notes for Teams
+
+## 1.0.0
+- Initial release
---
+++ src/models/Settings.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace webhubworks\teams\models;
+
+use Craft;
+use craft\base\Model;
+
+/**
+ * Teams settings
+ */
+class Settings extends Model
+{
+}
